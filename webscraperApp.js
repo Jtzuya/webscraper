1b130d0d3231bf21c2dd97d1af6374a126d99e8c
jtzuya <jtzuya@gmail.com>
2022-09-17 16:16:16 +0800

    updated webscraperapp.js file" -m "changing variable names" q ">" q \ ;[[;//..,/

--- webscraperApp.js
+++ webscraperApp.js
@@ -12,23 +12,23 @@ const productArr = []
 axios(url)
 .then(response => {
     const siteData = response.data;
-    const buzz = cheerio.load(siteData)
+    const func = cheerio.load(siteData)
 
     /*
         ================================================================
                                 FOR SHOPIFY
         ================================================================
     */
-    buzz('#CollectionSection .grid__item', siteData).each(function(){
+    func('#CollectionSection .grid__item', siteData).each(function(){
             const mainUrl = process.env.SECRET_MAINURL
-            const title = buzz(this).find('.grid-product__title').text()
+            const title = func(this).find('.grid-product__title').text()
 
             // URL configuration
-            const slug = buzz(this).find('.grid-product__link').attr('href')
+            const slug = func(this).find('.grid-product__link').attr('href')
             const url = mainUrl.concat(slug)
             
             // Price configuration
-            const price = buzz(this).find('.grid-product__price span').text()
+            const price = func(this).find('.grid-product__price span').text()
 
             // Image configuration
             // const images = buzz(this).find('[data-srcset]').attr('src')
@@ -38,7 +38,9 @@ axios(url)
                 url,
                 price
             })
-        })
-    }).catch((error) => console.log('zz'))
 
-app.listen(PORT, () => console.log(`Express server listening on port ${PORT}`))
+            console.log(productArr)
+        })
+    }).catch(() => console.log('Something went wrong!'))
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
